27265d334aa954152bb6ff9e2d7fc028a463d2da
Spencer WAWAKU <spencerm365@outlook.fr>
2025-09-16 22:37:39 +0200

    add the first project

--- 2_SCRIMBA/src/components/project_1/MainContent_1.jsx
+++ 2_SCRIMBA/src/components/project_1/MainContent_1.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
-
+import logo from '../../assets/react.svg'
 function MainContent() {
   return (
     <div className='mt-[100px] px-[64px]'>
       <p className='mt-6 text-5xl font-bold'>Fun Facts about React!</p>
-      <ol className='mt-6 text-lg list-decimal list-inside'>
+      <ol className='mt-6 text-lg list-disc list-inside marker:text-[#61DAFB]'>
         <li>Was first released in 2013</li>
         <li>Was originally created by Jordan Walker</li>
         <li>Has well over 200k stars on Github</li>
         <li>Is maintained by Meta</li>
         <li>Powers thousands of enterprise apps, including</li>
       </ol>
+        <img src={logo} alt="logo image" className='absolute w-[400px] h-[400px] top-1/9 right-[8%] p-[24px] text-neutral-700' style={{ filter: 'grayscale(100%) brightness(30%)' }}/>
     </div>
   )
 }
--- 2_SCRIMBA/src/components/project_1/Navbar_1.jsx
+++ 2_SCRIMBA/src/components/project_1/Navbar_1.jsx
@@ -5,11 +5,15 @@ const Navbar_1 = () => {
   return (
     <>
       <nav className='w-full h-12 bg-blue-950 flex items-center justify-between py-[4px] px-[64px]'>
-        <div className='flex items-center justify-start space-x-2'>
+        <div className='flex items-center justify-start space-x-2 shadow-lg'>
             <img src={logo} alt="React Logo" className='w-8 h-8' />
         <h1 className='text-lg font-bold text-cyan-400'>React Facts</h1>
         </div>
-
+          <ul className='nav-list flex flex-row justify-center items-center gap-4 '>
+            <li className='nav-list text-md font-medium cursor-pointer'>Pricing</li>
+            <li className='nav-list text-md font-medium cursor-pointer'>About</li>
+            <li className='nav-list text-md font-medium cursor-pointer'>Contact</li>
+          </ul>
       </nav>
     </>
   )
